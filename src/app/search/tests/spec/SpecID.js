0794eb9fa8545f0e958f731f154e974e72cbd655
Scott Davis <stdavis@utah.gov>
2014-04-24 10:27:03 -0600

    ID search widget wired up

---
+++ src/app/search/tests/spec/SpecID.js
@@ -0,0 +1,51 @@
+require([
+    'app/search/ID',
+
+    'dojo/_base/window',
+
+    'dojo/dom-construct'
+], function(
+    WidgetUnderTest,
+
+    win,
+
+    domConstruct
+) {
+    describe('app/search/ID', function() {
+        var widget;
+        var destroy = function (widget) {
+            widget.destroyRecursive();
+            widget = null;
+        };
+
+        beforeEach(function() {
+            widget = new WidgetUnderTest(null, domConstruct.create('div', null, win.body()));
+            widget.startup();
+        });
+
+        afterEach(function() {
+            if (widget) {
+                destroy(widget);
+            }
+        });
+
+        describe('Sanity', function() {
+            it('should create a ID', function() {
+                expect(widget).toEqual(jasmine.any(WidgetUnderTest));
+            });
+        });
+        describe('getSearchParam', function () {
+            it('returns search text', function () {
+                var value = 'blah';
+                widget.textBox.value = value;
+
+                expect(widget.getSearchParam()).toEqual(value);
+            });
+            it('throws error', function () {
+                expect(function () {
+                    widget.getSearchParam();
+                }).toThrow(widget.invalidMsg);
+            });
+        });
+    });
+});
