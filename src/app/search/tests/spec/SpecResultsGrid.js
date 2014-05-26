e4777c2dd245714243a1c324d46aaec727910015
Scott Davis <stdavis@utah.gov>
2014-05-26 10:58:11 -0600

    Show search results in grid
    Search results are now displayed in grid grouped by query layer.
    
    Closes #71

--- src/app/search/tests/spec/SpecResultsGrid.js
+++ src/app/search/tests/spec/SpecResultsGrid.js
@@ -1,18 +1,27 @@
 require([
     'app/search/ResultsGrid',
+    'app/config',
 
     'dojo/_base/window',
 
-    'dojo/dom-construct'
+    'dojo/dom-construct',
+    'dojo/text!app/search/tests/data/results.json'
 ], function(
     WidgetUnderTest,
+    config,
 
     win,
 
-    domConstruct
+    domConstruct,
+    resultsTxt
 ) {
 
     var widget;
+    var testdata = JSON.parse(resultsTxt);
+
+    beforeEach(function() {
+        widget = new WidgetUnderTest(null, domConstruct.create('div', null, win.body()));
+    });
 
     afterEach(function() {
         if (widget) {
@@ -23,12 +32,28 @@ require([
 
     describe('app/search/ResultsGrid', function() {
         describe('Sanity', function() {
-            beforeEach(function() {
-                widget = new WidgetUnderTest(null, domConstruct.create('div', null, win.body()));
-            });
-
             it('should create a ResultsGrid', function() {
                 expect(widget).toEqual(jasmine.any(WidgetUnderTest));
+            });
+        });
+        describe('onFeaturesFound', function () {
+
+        });
+        describe('getStoreData', function () {
+            it('returns a flattened array of data', function () {
+                config.queryLayerNames = {};
+                config.queryLayerNames['7'] = 'blah';
+                config.queryLayerNames['15'] = 'blah2';
+                var result = widget.getStoreData(testdata);
+
+                expect(result.length).toBe(22);
+
+                var r = result[3];
+
+                expect(r.attributes).toBeUndefined();
+                expect(r[config.fieldNames.queryLayers.ID]).toBeDefined();
+                expect(r.parent).toBe('blah');
+                expect(result[result.length - 1].parent).toBe('blah2');
             });
         });
     });
