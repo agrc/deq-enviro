e1984edad84bfda162a479823173064468ec97ba
Scott Davis <stdavis@utah.gov>
2014-06-13 10:04:15 -0600

    Added unique id field to grid and store.
    This fixes a bug that was preventing multiple no features found rows from displaying.
    
    It will also make sure that ID between layers don't collide and generally make me feel happy. :)

--- src/app/search/tests/spec/SpecResultsGrid.js
+++ src/app/search/tests/spec/SpecResultsGrid.js
@@ -46,11 +46,15 @@ require([
             });
         });
         describe('getStoreData', function () {
-            it('returns a flattened array of data', function () {
+            var result;
+            beforeEach(function () {
                 config.queryLayerNames = {};
                 config.queryLayerNames['7'] = 'blah';
                 config.queryLayerNames['15'] = 'blah2';
-                var result = widget.getStoreData(testdata);
+                config.queryLayerNames['5'] = 'blah3';
+                result = widget.getStoreData(testdata);
+            });
+            it('returns a flattened array of data', function () {
 
                 expect(result.length).toBe(22);
 
@@ -60,6 +64,19 @@ require([
                 expect(r[config.fieldNames.queryLayers.ID]).toBeDefined();
                 expect(r.parent).toBe('blah');
                 expect(result[result.length - 1].parent).toBe('blah2');
+            });
+            it('creates a unique id field that has a unique id for each feature', function () {
+                var fld = config.fieldNames.queryLayers.UNIQUE_ID;
+
+                // header
+                expect(result[0][fld]).toEqual('5');
+
+                // no feature found
+                expect(result[1][fld]).toEqual('5-' + config.messages.noFeaturesFound);
+
+                // features
+                expect(result[3][fld]).toEqual('7-4302110758');
+                expect(result[20][fld]).toEqual('15-84720MRCNZ10622');
             });
         });
     });
