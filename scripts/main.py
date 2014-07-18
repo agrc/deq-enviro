dc3cb4035d3fde9616886bb54eb50d7faaecc105
Scott Davis <stdavis@utah.gov>
2014-07-18 11:40:43 -0600

    Added better logging and error checking

--- scripts/main.py
+++ scripts/main.py
@@ -4,17 +4,47 @@ import agrc.logging
 import build_json
 import update_sgid
 import update_fgdb
+from agrc import messaging
 
-logger = agrc.logging.Logger()
+emailer = None
+logger = None
+scriptName = 'DEQ Nightly'
 
-logger.logMsg('building json')
-build_json.run()
+def run():
+    global emailer, logger
+    errors = []
 
-logger.logMsg('updating SGID')
-update_sgid.run()
+    emailer = messaging.Emailer(['stdavis@utah.gov', 'haroldsandbeck@utah.gov'], testing=False)
 
-logger.logMsg('updating FGDB')
-update_fgdb.run()
+    logger = agrc.logging.Logger()
 
-logger.writeLogToFile()
-print('done')
+    logger.logMsg('\n\n***** BUILDING JSON FILE')
+    build_json.run()
+
+    logger.logMsg('\n\n***** UPDATING SGID')
+    errors = errors + update_sgid.run(logger)
+
+    logger.logMsg('\n\n***** UPDATING FILE GEODATABASE')
+    errors = errors + update_fgdb.run(logger)
+    
+    logger.logMsg('\n\n***** Script completed successfully.')
+
+    logger.writeLogToFile()
+
+    if len(errors) > 0:
+        emailer.sendEmail('{} - Data Errors'.format(scriptName), 'There were errors in the nightly deq script: \n{}'.format('\n\n'.join(errors)))
+    else:
+        emailer.sendEmail('{} - Script Ran Successfully'.format(scriptName), 'Do you want the log here?')
+
+def run_with_try_catch():
+    try:
+        run()
+    except Exception:
+        logger.logError()
+        emailer.sendEmail('{} - Script Error'.format(scriptName), logger.log)
+    finally:
+        logger.writeLogToFile()
+        
+if __name__ == "__main__":
+    run_with_try_catch()
+#     run()
--- scripts/update_fgdb.py
+++ scripts/update_fgdb.py
@@ -1,3 +1,5 @@
+# copies data from SGID to the app and makes necessary optimizations
+
 import arcpy
 import settings
 from settings import fieldnames
@@ -11,40 +13,71 @@ fiveFields = [
               fieldnames.CITY, 
               fieldnames.TYPE
               ]
+logger = None
+errors = []
 
-def run(logger):
+def run(logr):
+    global logger, errors
+    logger = logr
+    
+    logger.logMsg('processing query layers')
+    update_query_layers()
+    
+    logger.logMsg('processing related tables')
+    update_related_tables()
+    
+    return errors
+    
+def update_related_tables():
+    for t in spreadsheet.get_related_tables():
+        name = t[fieldnames.sgidName]
+        try:
+            if name.startswith('SGID10'):
+                logger.logMsg('Processing: {}'.format(name.split('.')[-1]))
+                localTbl = path.join(settings.fgd, name.split('.')[-1])
+                remoteTbl = path.join(settings.sgid, name)
+                update(localTbl, remoteTbl)
+        except:
+            errors.append('Execution error trying to update fgdb with {}:\n{}'.format(name, logger.logError()))
+    
+def update(local, remote):
+    logger.logMsg('\n\nupdating: {} \n    from: {}'.format(local, remote))
+    if not arcpy.Exists(local):
+        logger.logMsg('creating new local feature class')
+        arcpy.Copy_management(remote, local)
+    else:
+        arcpy.TruncateTable_management(local)
+        arcpy.Append_management(remote, local, 'NO_TEST')
+        
+def update_query_layers():
     for l in spreadsheet.get_query_layers():
         fcname = l[fieldnames.sgidName]
-        # only try to update rows with valid sgid names and data sources
-        if fcname.startswith('SGID10.ENVIRONMENT'):
-            # update fgd from SGID
-            logger.logMsg('Processing: {}'.format(fcname.split('.')[-1]))
-            localFc = path.join(settings.fgd, fcname.split('.')[-1])
-            remoteFc = path.join(settings.sgid, fcname)
-            if not arcpy.Exists(localFc):
-                logger.logMsg('creating new local feature class')
-                arcpy.Copy_management(remoteFc, localFc)
-            else:
-                pass
-                arcpy.TruncateTable_management(localFc)
-                arcpy.Append_management(remoteFc, localFc, 'NO_TEST')
-            
-            # APP-SPECIFIC OPTIMIZATIONS
-            # make sure that it has the five main fields for the fdg only
-            upper_fields = [x.name.upper() for x in arcpy.ListFields(localFc)]      
-            for f in fiveFields:
-                if not f in upper_fields:
-                    logger.logMsg('{} not found. Adding to {}'.format(f, localFc))
-                    arcpy.AddField_management(localFc, f, 'TEXT', 0, 0, 500)
-            
-                # calc field
-                expression = l[f]
-                if not expression == 'n/a':
-                    expression = '!{}!'.format(expression)
-                else:
-                    expression = '"{}"'.format(expression)
-                arcpy.CalculateField_management(localFc, f, expression, 'PYTHON')    
-                    
+        try:
+            # only try to update rows with valid sgid names and data sources
+            if fcname.startswith('SGID10'):
+                # update fgd from SGID
+                logger.logMsg('Processing: {}'.format(fcname.split('.')[-1]))
+                localFc = path.join(settings.fgd, fcname.split('.')[-1])
+                remoteFc = path.join(settings.sgid, fcname)
+                update(localFc, remoteFc)
+                
+                # APP-SPECIFIC OPTIMIZATIONS
+                # make sure that it has the five main fields for the fdg only
+                upper_fields = [x.name.upper() for x in arcpy.ListFields(localFc)]      
+                for f in fiveFields:
+                    if not f in upper_fields:
+                        logger.logMsg('{} not found. Adding to {}'.format(f, localFc))
+                        arcpy.AddField_management(localFc, f, 'TEXT', 0, 0, 500)
+                
+                    # calc field
+                    expression = l[f]
+                    if not expression == 'n/a':
+                        expression = '!{}!'.format(expression)
+                    else:
+                        expression = '"{}"'.format(expression)
+                    arcpy.CalculateField_management(localFc, f, expression, 'PYTHON')    
+        except:
+            errors.append('Execution error trying to update fgdb with {}:\n{}'.format(fcname, logger.logError()))
 
 if __name__ == '__main__':
     from agrc import logging
