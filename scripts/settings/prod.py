b99088119a26ea300fa934a9b90030e2758fbf14
Scott Davis <stdavis@utah.gov>
2014-07-18 16:12:29 -0600

    Clean up and settings

--- scripts/settings/prod.py
+++ scripts/settings/prod.py
@@ -1,5 +1,5 @@
 from dev import *
 
 # overrides for production environment
-webdata = r'\\172.16.17.57\ArcGISServer\data\webdata' # production
+webdata = r'C:\MapData\webdata' # specific to .57
 sgid = os.path.join(dbConnects, 'SGID10 as ENVIRONMENT on prod.sde')
