784604ecd57c003a51d17d5ab857110594155b5c
Scott Davis <stdavis@utah.gov>
2014-07-21 14:09:03 -0600

    sendEmails in settings

--- scripts/settings/prod.py
+++ scripts/settings/prod.py
@@ -3,3 +3,5 @@ from dev import *
 # overrides for production environment
 webdata = r'C:\MapData\webdata' # specific to .57
 sgid = os.path.join(dbConnects, 'SGID10 as ENVIRONMENT on prod.sde')
+
+sendEmails = True
