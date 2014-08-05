339c88028608e28f76126dd7f9a55fb16280c303
Scott Davis <stdavis@utah.gov>
2014-08-05 16:35:49 -0600

    Improved handling of Identify Attributes fields.
    Script now validates field names and returns errors for those it cannot find. Closes #141
    
    Fixed bug in script that was causing it to choke on field aliases with parentheses in them. Closes #135

--- scripts/tests/test_build_json.py
+++ scripts/tests/test_build_json.py
@@ -8,8 +8,8 @@ class BuildJSONTests(unittest.TestCase):
     def test_builds_json_object(self):
         json = build_json.run()
         
-        self.assertEqual(len(json[fieldnames.queryLayers]), 32)
-        self.assertEqual(len(json[fieldnames.otherLinks]), 1)
+        self.assertEqual(len(json[fieldnames.queryLayers]), 34)
+        self.assertEqual(len(json[fieldnames.otherLinks]), 2)
         
     def test_adds_layer_indices(self):
         lyr = build_json.run()[fieldnames.queryLayers][15]
@@ -23,3 +23,12 @@ class BuildJSONTests(unittest.TestCase):
         self.assertEqual(len(result), 3)
         self.assertEqual(result[0], ['SYSFACID', 'System-Facility ID'])
         self.assertEqual(result[2], ['FACACTIVITY', 'Facility Activity Status'])
+        
+    def test_parse_fields_handles_nested_parentheses(self):
+        txt = 'Easting (UTM_Easting), Year (Year), Carbon_Monoxide (Carbon Monoxide (TONS/YR)), Oxides_Nitrogen (Oxides of Nitrogen (TONS/YR))'
+        
+        result = build_json.parse_fields(txt)
+        
+        self.assertEqual(result[2], ['Carbon_Monoxide', 'Carbon Monoxide (TONS/YR)'])
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result[0], ['Easting', 'UTM_Easting'])
