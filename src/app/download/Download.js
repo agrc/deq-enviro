d53f5902299718217dd89b151f55ea1b7844d776
steveoh <sgourley@utah.gov>
2014-08-26 15:40:04 -0600

    basic ui

--- src/app/download/Download.js
+++ src/app/download/Download.js
@@ -3,28 +3,40 @@ define([
 
     'dojo/_base/declare',
 
+    'dojo/dom-class',
+
+    'dojo/topic',
+
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
-    'dijit/_WidgetsInTemplateMixin'
+
+    'app/config'
 ], function(
     template,
 
     declare,
 
+    domClass,
+
+    topic,
+
     _WidgetBase,
     _TemplatedMixin,
-    _WidgetsInTemplateMixin
+
+    config
 ) {
-    return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+    return declare([_WidgetBase, _TemplatedMixin], {
         // description:
         //      Hooks up to the search results grid and makes requests to a
         //      geoprocessing service to download data. issue #143
 
         templateString: template,
         baseClass: 'download',
-        widgetsInTemplate: true,
 
-        // Properties to be sent into constructor
+        _setCountAttr: {
+            node: 'countNode',
+            type: 'innerHTML'
+        },
 
         postCreate: function() {
             // summary:
@@ -43,6 +55,48 @@ define([
             //
             console.log('app.download.Download::setupConnections', arguments);
 
+            var self = this;
+            this.own(
+                topic.subscribe(config.topics.appSearch.featuresFound, function(t) {
+                    self.updateCount(t);
+                }),
+                this.watch('count', function(name, original, change){
+                    self.updateVisibility(change);
+                })
+            );
+        },
+        updateCount: function(data) {
+            // summary:
+            //      receives the data from the search xhr and sets the count
+            //      value of the label to the number of results
+            // data - xhr search result from appSearch.featuresFound topic
+            console.log('app.download.Download::updateCount', arguments);
+
+            var count = 0;
+            for (var key in data) {
+                // make sure it's not from the prototype!
+                if (data.hasOwnProperty(key)) {
+                    count += data[key].length;
+                }
+            }
+
+            this.set('count', count);
+
+            return count;
+        },
+        updateVisibility: function(value) {
+            // summary:
+            //      enables and disables the download button based on result counts
+            // value
+            console.log('app.download.Download::updateButtonState', arguments);
+
+            if(value > 0)
+            {
+                domClass.replace(this.domNode, 'show', 'hidden');
+                return;
+            }
+
+            domClass.replace(this.domNode, 'hidden', 'show');
         }
     });
 });
