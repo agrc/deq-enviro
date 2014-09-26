017cf997c0c8c4d298ac6b6b972776c1351ce4e7
Scott Davis <stdavis@utah.gov>
2014-09-26 14:01:00 -0600

    Validation and data submission for request form wired up.
    Closes #195

--- src/app/security/_RequestPane.js
+++ src/app/security/_RequestPane.js
@@ -2,18 +2,34 @@ define([
     'dojo/text!./templates/_RequestPane.html',
 
     'dojo/_base/declare',
+    'dojo/_base/array',
+    'dojo/_base/lang',
     'dojo/dom-style',
     'dojo/aspect',
+    'dojo/string',
+    'dojo/query',
 
-    'ijit/widgets/authentication/_LoginRegisterRequestPane'
+    'put-selector/put',
+
+    'ijit/widgets/authentication/_LoginRegisterRequestPane',
+
+    'app/config'
 ], function(
     template,
 
     declare,
+    array,
+    lang,
     domStyle,
     aspect,
+    dojoString,
+    query,
 
-    _LoginRegisterRequestPane
+    put,
+
+    _LoginRegisterRequestPane,
+
+    config
 ) {
     return declare([_LoginRegisterRequestPane], {
         // description:
@@ -31,8 +47,42 @@ define([
             // summary:
             //      description
             console.log('app/security/_RequestPane:getData', arguments);
+            var layers = [];
 
+            query('input', this.layersContainer).forEach(function (node) {
+                if (node.checked) {
+                    layers.push(node.value);
+                }
+            });
+            var options = {
+                address: this.addressTxt.value,
+                city: this.cityTxt.value,
+                state: this.stateTxt.value,
+                phone: this.phoneTxt.value,
+                zip: this.zipTxt.value,
+                layers: layers,
+                locationTxt: this.locationTxt.value,
+                timePeriod: this.timeSelect.value
+            };
 
+            return lang.mixin({options: options}, this.inherited(arguments));
+        },
+        validate: function () {
+            // summary:
+            //      overrides to make additional fields required
+            console.log('app/security/_RequestPane:validate', arguments);
+
+            arguments[0].charCode = null;
+            arguments[0].keyCode = null;
+
+            var valid = (this.timeSelect.value !== '' &&
+                query('input', this.layersContainer).some(function (node) {
+                    return node.checked;
+                }) &&
+                this.locationTxt.value.length > 0
+            );
+
+            return valid && this.inherited(arguments);
         },
         postCreate: function () {
             // summary:
@@ -47,6 +97,17 @@ define([
                 var width = (pane === that) ? that.width + 'px' : '';
                 domStyle.set(that.parentWidget.modalDialog, 'width', width);
             }, true);
+
+            // load available secured layers
+            config.getAppJson().then(function (json) {
+                array.forEach(json.queryLayers, function (ql) {
+                    if (ql.secure === 'Yes') {
+                        var putString = 'li.checkbox label input[type=checkbox][value=${index}]+span';
+                        put(that.layersContainer, dojoString.substitute(putString, ql), ql.name);
+                    }
+                });
+                query('input', that.layersContainer).on('change', lang.hitch(that, 'validate'));
+            });
         }
     });
 });
--- src/app/security/tests/spec/Spec_RequestPane.js
+++ src/app/security/tests/spec/Spec_RequestPane.js
@@ -1,11 +1,19 @@
 require([
     'app/security/_RequestPane',
 
-    'dojo/dom-construct'
+    'dojo/dom-construct',
+    'dojo/Deferred',
+    'dojo/query',
+
+    'app/config'
 ], function(
     WidgetUnderTest,
 
-    domConstruct
+    domConstruct,
+    Deferred,
+    query,
+
+    config
 ) {
     describe('app/security/_RequestPane', function() {
         var widget;
@@ -15,6 +23,21 @@ require([
         };
 
         beforeEach(function() {
+            var def = new Deferred();
+            spyOn(config, 'getAppJson').and.returnValue(def);
+            def.resolve({
+                queryLayers: [{
+                    secure: 'No'
+                }, {
+                    secure: 'Yes',
+                    name: 'Layer Name',
+                    index: 1
+                }, {
+                    secure: 'Yes',
+                    name: 'Layer Name Two',
+                    index: 2
+                }]
+            });
             widget = new WidgetUnderTest({
                 parentWidget: {goToPane: function () {}}
             }, domConstruct.create('div', null, document.body));
@@ -29,6 +52,84 @@ require([
         describe('Sanity', function() {
             it('should create a _RequestPane', function() {
                 expect(widget).toEqual(jasmine.any(WidgetUnderTest));
+            });
+        });
+        describe('postCreat', function () {
+            it('should create the layer checkboxes', function () {
+                expect(widget.layersContainer.children.length).toBe(2);
+
+                var lyr = widget.layersContainer.children[0];
+
+                expect(lyr.children[0].children[0].value).toBe('1');
+                expect(lyr.children[0].children[1].innerHTML).toBe('Layer Name');
+            });
+        });
+        describe('validate', function () {
+            it('requires all fields', function () {
+                widget.fNameTxt.value = 'test';
+                widget.lNameTxt.value = 'test';
+                widget.agencyTxt.value = 'test';
+                widget.addressTxt.value = 'test';
+                widget.cityTxt.value = 'test';
+                widget.stateTxt.value = 'test';
+                widget.phoneTxt.value = 'test';
+                widget.zipTxt.value = 'test';
+                widget.emailTxt.value = 'test@test.com';
+                widget.emailConfirmTxt.value = 'test@test.com';
+                widget.passwordTxt.value = 'pass';
+                widget.passwordConfirmTxt.value = 'pass';
+
+                expect(widget.validate({})).toBe(false);
+
+                widget.timeSelect.value = '3';
+
+                expect(widget.validate({})).toBe(false);
+
+                query('input', widget.layersContainer)[0].checked = true;
+
+                expect(widget.validate({})).toBe(false);
+
+                widget.locationTxt.value = 'blah';
+
+                expect(widget.validate({})).toBe(true);
+            });
+        });
+        describe('getData', function () {
+            it('get\'s extra fields', function () {
+                widget.fNameTxt.value = 'first';
+                widget.lNameTxt.value = 'last';
+                widget.agencyTxt.value = 'agency';
+                widget.addressTxt.value = 'address';
+                widget.cityTxt.value = 'city';
+                widget.stateTxt.value = 'state';
+                widget.phoneTxt.value = 'phone';
+                widget.zipTxt.value = 'zip';
+                widget.emailTxt.value = 'test@test.com';
+                widget.emailConfirmTxt.value = 'test@test.com';
+                widget.passwordTxt.value = 'pass';
+                widget.passwordConfirmTxt.value = 'pass';
+                query('input', widget.layersContainer)[0].checked = true;
+                query('input', widget.layersContainer)[1].checked = true;
+                widget.locationTxt.value = 'blah';
+                widget.timeSelect.value = '3';
+
+                expect(widget.getData()).toEqual({
+                    first: 'first',
+                    last: 'last',
+                    agency: 'agency',
+                    email: 'test@test.com',
+                    password: 'pass',
+                    options: {
+                        address: 'address',
+                        city: 'city',
+                        state: 'state',
+                        phone: 'phone',
+                        zip: 'zip',
+                        layers: ['1', '2'],
+                        locationTxt: 'blah',
+                        timePeriod: '3'
+                    }
+                });
             });
         });
     });
