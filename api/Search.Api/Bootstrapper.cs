10845a035b38443dd2e6f1b9aaddad741c42cde5
steveoh <sgourley@utah.gov>
2014-10-13 14:59:57 -0600

    search api improvements
    addresses: #227

--- api/Search.Api/Bootstrapper.cs
+++ api/Search.Api/Bootstrapper.cs
@@ -1,8 +1,12 @@
-﻿using Nancy;
+﻿using System.Net;
+using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.TinyIoc;
 using Newtonsoft.Json;
+using Raven.Client;
+using Raven.Client.Document;
 using Search.Api.Config;
+using Search.Api.Index;
 using Search.Api.Serializers;
 using Search.Api.Services;
 
@@ -11,17 +15,46 @@ namespace Search.Api {
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
-        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines) {
+        public Bootstrapper()
+        {
+            // Configure Store to point to your external RavenDB instance.  
+            // For simpliclity, we'll use an embedded instance instead
+            Store = new DocumentStore
+                {
+                    ConnectionStringName = "RavenDb"
+                }.Initialize();
+
+            Store.JsonRequestFactory.ConfigureRequest +=
+                (sender, args) =>
+                    {
+                        args.Request.PreAuthenticate = true;
+                        ((HttpWebRequest) args.Request).UnsafeAuthenticatedConnectionSharing = true;
+                    };
+
+            RavenConfig.Register(typeof (UserByIdIndex), Store);
+        }
+
+        public virtual IDocumentStore Store { get; private set; }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
             base.ApplicationStartup(container, pipelines);
 
             AutoMapperConfig.RegisterMaps();
         }
 
-        protected override void ConfigureApplicationContainer(TinyIoCContainer container) {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
             base.ConfigureApplicationContainer(container);
 
             container.Register(typeof (JsonSerializer), typeof (JsonSerializerOptions));
             container.Register<IQuerySoeService>(new QuerySoeServiceAsync());
+        }
+
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
+            container.Register(Store.OpenSession());
         }
     }
 }
