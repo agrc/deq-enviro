25afa85702f4e86420586961acba894d93b6a185
steveoh <sgourley@utah.gov>
2014-10-14 12:50:33 -0600

    queries work for only secured layers

--- api/Search.Api/Config/AutoMapperConfig.cs
+++ api/Search.Api/Config/AutoMapperConfig.cs
@@ -13,15 +13,6 @@ namespace Search.Api.Config {
         /// Registers the maps.
         /// </summary>
         public static void RegisterMaps() {
-            //      Mapper.CreateMap<ReverseGeocodeResponse, ReverseGeocodeResult>()
-            //            .ForMember(dest => dest.InputLocation, opt => opt.MapFrom(src => src.Location));
-            //
-            //      Mapper.CreateMap<GeocodeMilepostResponse, RouteMilepostResult>()
-            //            .ForMember(dest => dest.Source, opt => opt.MapFrom(src => src.Geocoder))
-            //            .ForMember(dest => dest.MatchRoute, opt => opt.MapFrom(src => src.MatchAddress))
-            //            .ForMember(d => d.InputRouteMilePost, src => src.Ignore())
-            //            .ForMember(dest => dest.Location, opt => opt.ResolveUsing<LocationFromGeocodeMilepostResolver>());
-
             Mapper.CreateMap<SearchRequest, SoeSearchRequest>()
                   .ForMember(dest => dest.DefinitionQueries,
                              option => option.MapFrom(src => src.QueryLayers.Select(x => x.DefQuery).ToArray()))
