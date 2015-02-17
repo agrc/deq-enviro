3c709fefe7fe5da2c6ec173360d4e14cf2673696
Scott Davis <stdavis@utah.gov>
2015-02-17 15:44:27 -0700

    prod settings

--- scripts/nightly/settings/prod.py
+++ scripts/nightly/settings/prod.py
@@ -1,3 +1,6 @@
 from stage import *
 
 # overrides for production environment
+sgid = {'ENVIRONMENT': os.path.join(dbConnects, 'SGID10 as ENVIRONMENT on prod.sde'),
+    'WATER': os.path.join(dbConnects, 'SGID10 as WATER on prod.sde'),
+    'ENERGY': os.path.join(dbConnects, 'SGID10 as ENERGY on prod.sde')}
