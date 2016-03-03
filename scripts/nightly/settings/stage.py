0e85fa862612908318ba71430b17608d31bd6237
Scott Davis <stdavis@utah.gov>
2016-03-03 16:26:38 -0700

    fix path for stage

--- scripts/nightly/settings/stage.py
+++ scripts/nightly/settings/stage.py
@@ -1,7 +1,7 @@
 from dev import *
 
 # overrides for stage environment
-webdata = r'C:\MapData\webdata' # specific to .58
+webdata = r'C:\inetpub\wwwroot\deqenviro\webdata' # specific to .58
 sgid = {'ENVIRONMENT': os.path.join(dbConnects, 'SGID10 as ENVIRONMENT on stage.sde'),
     'WATER': os.path.join(dbConnects, 'SGID10 as WATER on stage.sde'),
     'ENERGY': os.path.join(dbConnects, 'SGID10 as ENERGY on stage.sde')}
