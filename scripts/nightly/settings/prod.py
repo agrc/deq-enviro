b185d31950d46f5e954514caa6eb747382021fd6
Scott Davis <stdavis@utah.gov>
2017-03-21 07:59:34 -0600

    switch to prod ip address

--- scripts/nightly/settings/prod.py
+++ scripts/nightly/settings/prod.py
@@ -6,8 +6,10 @@ sgid = {'ENVIRONMENT': os.path.join(dbConnects, 'SGID10 as ENVIRONMENT on prod.s
         'ENERGY': os.path.join(dbConnects, 'SGID10 as ENERGY on prod.sde')}
 updateFTP = True
 queryLayersUrl = 'https://docs.google.com/spreadsheets/d/1-HJViZduddtnxSq35yzhR6YU0U6Pwo0OaimXPHT4vPw/edit#gid=0'
-mapServiceJson = 'http://mapserv.utah.gov/arcgis/rest/services/DEQEnviro/MapService/MapServer?f=json'
-securedServiceJson = 'http://mapserv.utah.gov/arcgis/rest/services/DEQEnviro/Secure/MapServer?f=json'
+
+agsServer = '172.16.17.51'
+mapServiceJson = 'http://172.16.17.51:6080/arcgis/rest/services/DEQEnviro/MapService/MapServer?f=json'
+securedServiceJson = 'http://172.16.17.51:6080/arcgis/rest/services/DEQEnviro/Secure/MapServer?f=json'
 sendEmails = True
 
 reportEmail = 'haroldsandbeck@utah.gov'
