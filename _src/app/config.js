0c223baf025379ffc737a64a46fc37703e576237
Scott Davis <stdavis@utah.gov>
2017-06-06 15:34:02 -0600

    Release v1.3.0-4

--- _src/app/config.js
+++ _src/app/config.js
@@ -88,7 +88,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.3.0-3',
+        version: '1.3.0-4',
 
         // popupDelay: Number
         //      The delay (in milliseconds) before a popup is shown on hover.
