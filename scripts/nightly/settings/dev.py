9d2c2b99b619586142856b918f3c194036b0059b
Scott Davis <stdavis@utah.gov>
2021-06-15 09:09:48 -0600

    feat: add ddw water system ratings query layer
    Closes #419

--- scripts/nightly/settings/dev.py
+++ scripts/nightly/settings/dev.py
@@ -13,6 +13,7 @@ dbConnects = os.path.join(os.path.dirname(os.path.abspath(__file__)), r'..\datab
 sgid = {'ENVIRONMENT': os.path.join(dbConnects, 'SGID as ENVIRONMENT on local.sde'),
         'WATER': os.path.join(dbConnects, 'SGID as WATER on local.sde'),
         'ENERGY': os.path.join(dbConnects, 'SGID as ENERGY on local.sde')}
+utahddw_file_name = 'utahddw_at.sde'
 
 fgd = 'deqquerylayers.gdb'
 
