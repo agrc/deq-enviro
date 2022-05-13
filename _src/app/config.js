31ec42acdfb7e11eff77c289e97ff6923d247cff
stdavis <stdavis@utah.gov>
2022-05-13 15:48:21 -0600

    Release v1.8.6-1

--- _src/app/config.js
+++ _src/app/config.js
@@ -92,7 +92,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.8.6-0',
+        version: '1.8.6-1',
 
         // popupDelay: Number
         //      The delay (in milliseconds) before a popup is shown on hover.
