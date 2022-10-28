71cdc741520e8a2b9d9de39b054f2151e73d6336
agrc developers <agrc-dev@utah.gov>
2022-10-28 11:57:55 -0600

    fix: remove old IPs and move to ignored settings file

--- scripts/nightly/settings/prod.py
+++ scripts/nightly/settings/prod.py
@@ -7,13 +7,6 @@ sgid = {'ENVIRONMENT': os.path.join(dbConnects, 'SGID as ENVIRONMENT on prod.sde
 queryLayersUrl = 'https://docs.google.com/spreadsheets/d/1-HJViZduddtnxSq35yzhR6YU0U6Pwo0OaimXPHT4vPw/edit#gid=0'
 utahddw_file_name = 'utahddw.sde'
 
-agsServer = '172.16.17.53'
-mapServiceJson = 'http://172.16.17.53:6080/arcgis/rest/services/DEQEnviro/MapService/MapServer?f=json'
-securedServiceJson = 'http://172.16.17.53:6080/arcgis/rest/services/DEQEnviro/Secure/MapServer?f=json'
 sendEmails = True
 
-reportEmail = ['haroldsandbeck@utah.gov', 'mhorning@utah.gov']
-
-webdata = r'\\172.16.77.69\c$\inetpub\wwwroot\deqenviro\webdata'
-mapData1 = r'\\172.16.17.53\c$\MapData'
-mapData2 = r'\\172.16.17.54\c$\MapData'
+reportEmail = ['mhorning@utah.gov']
