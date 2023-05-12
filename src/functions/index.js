afd2793388ca3078ef3abb1320c1e43f3977045b
stdavis <stdavis@utah.gov>
2023-05-12 11:48:41 -0600

    chore(functions): upgrade to v2 firebase functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,16 +1,14 @@
 import admin from 'firebase-admin';
-import functions from 'firebase-functions';
+import { onRequest } from 'firebase-functions/v2/https';
 import update from './update.js';
 
 admin.initializeApp();
 
-export const updateRemoteConfigFromSheets = functions.https.onRequest(
-  async (_, res) => {
-    try {
-      res.send(await update());
-    } catch (e) {
-      console.error('returning error', e);
-      res.status(500).send({ error: e.toString() });
-    }
+export const configs = onRequest(async (_, res) => {
+  try {
+    res.send(await update());
+  } catch (e) {
+    console.error('returning error', e);
+    res.status(500).send({ error: e.toString() });
   }
-);
+});
