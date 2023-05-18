dbac24645e72a730fe62500ee8cf1f9f97900e59
stdavis <stdavis@utah.gov>
2023-05-18 14:41:32 -0600

    feat(table): implement utds table

---
+++ src/utah-design-system/Table.stories.jsx
@@ -0,0 +1,52 @@
+import queryLayerResult from '../../tests/fixtures/queryLayerResult.json';
+import Table from './Table';
+
+export default {
+  title: 'Utah Design System/Table',
+  component: Table,
+};
+
+const Template = (args) => <Table {...args} caption="test caption" />;
+const columns = [
+  {
+    accessorKey: 'OBJECTID',
+  },
+  {
+    accessorKey: 'DERRID',
+  },
+  {
+    accessorKey: 'SITENAME',
+  },
+  {
+    accessorKey: 'SITEADDRES',
+  },
+  {
+    accessorKey: 'SITECITY',
+  },
+  {
+    accessorKey: 'SITEDESC',
+  },
+];
+
+export const Default = Template.bind({});
+Default.args = {
+  columns,
+  data: queryLayerResult.features.map((feature) => feature.attributes),
+  initialState: {
+    columnVisibility: {
+      OBJECTID: false,
+    },
+  },
+};
+
+export const Scrollable = Template.bind({});
+Scrollable.args = {
+  columns,
+  data: queryLayerResult.features.map((feature) => feature.attributes),
+  initialState: {
+    columnVisibility: {
+      OBJECTID: false,
+    },
+  },
+  className: 'h-80',
+};
