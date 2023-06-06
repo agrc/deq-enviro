023d7186f2a361d1efa342b03572e64feda2093e
stdavis <stdavis@utah.gov>
2023-06-06 11:50:51 -0600

    feat: add error boundary

---
+++ src/components/RootErrorFallback.jsx
@@ -0,0 +1,45 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
+import PropTypes from 'prop-types';
+import Button from '../utah-design-system/Button';
+
+export default function RootErrorFallback({ error }) {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="w-1/2 max-w-xl border border-slate-700 bg-red-50 p-6 transition">
+        <h3>Something went wrong</h3>
+        <p className="mt-1">
+          {error?.message ?? 'An unknown error occurred. Please try again.'}
+        </p>
+        <Collapsible.Root>
+          <div className="mt-4 flex w-full items-center justify-between space-x-4">
+            <Collapsible.Trigger asChild>
+              <Button
+                appearance={Button.Appearances.outlined}
+                color={Button.Colors.none}
+              >
+                Show technical details 🤓
+              </Button>
+            </Collapsible.Trigger>
+            <Button
+              appearance={Button.Appearances.solid}
+              color={Button.Colors.none}
+              onClick={() => window.location.reload()}
+            >
+              Reload
+            </Button>
+          </div>
+          <Collapsible.Content
+            asChild
+            className="mt-4 overflow-auto border border-slate-300 bg-slate-50 text-sm data-[state=closed]:animate-slide-up data-[state=open]:animate-slide-down"
+          >
+            <pre>{error?.stack}</pre>
+          </Collapsible.Content>
+        </Collapsible.Root>
+      </div>
+    </div>
+  );
+}
+
+RootErrorFallback.propTypes = {
+  error: PropTypes.object,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,32 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@utahdts/utah-design-system-header/css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { FirebaseAppProvider } from 'reactfire';
 import App from './App.jsx';
+import RootErrorFallback from './components/RootErrorFallback.jsx';
 import './index.css';
 
 const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <FirebaseAppProvider
-        firebaseConfig={{
-          apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-          appId: import.meta.env.VITE_FIREBASE_APP_ID,
-          authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-          messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-          projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-          storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-        }}
-      >
-        <App />
-      </FirebaseAppProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <ErrorBoundary FallbackComponent={RootErrorFallback}>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <FirebaseAppProvider
+          firebaseConfig={{
+            apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+            appId: import.meta.env.VITE_FIREBASE_APP_ID,
+            authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+            messagingSenderId: import.meta.env
+              .VITE_FIREBASE_MESSAGING_SENDER_ID,
+            projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+            storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+          }}
+        >
+          <App />
+        </FirebaseAppProvider>
+      </QueryClientProvider>
+    </React.StrictMode>
+  </ErrorBoundary>
 );
