5a781b5f516d8bbeb7887a69e64834d690801dba
stdavis <stdavis@utah.gov>
2023-06-15 10:24:33 -0600

    feat(input): implement utds input

---
+++ src/utah-design-system/Input.jsx
@@ -0,0 +1,103 @@
+import * as Label from '@radix-ui/react-label';
+import PropTypes from 'prop-types';
+import { twJoin, twMerge } from 'tailwind-merge';
+
+export default function Input({
+  className,
+  disabled,
+  id,
+  inline,
+  invalid,
+  label,
+  max,
+  message,
+  min,
+  onChange,
+  required,
+  step,
+  type = 'text',
+  value,
+}) {
+  if (!id) {
+    id = label.toLowerCase().replace(' ', '-');
+  }
+
+  return (
+    <div
+      className={twMerge(
+        inline ? 'inline-flex items-center' : 'flex flex-col',
+        className
+      )}
+    >
+      <Label.Root asChild className="mr-2" htmlFor={id}>
+        <strong>
+          {label}
+          {required && <span className="ml-[0.1rem] text-error-500">*</span>}
+        </strong>
+      </Label.Root>
+      <div className="flex flex-1 flex-col">
+        <div
+          className={twJoin(
+            '-m-1 rounded-md p-1',
+            !disabled && 'hover:bg-slate-200'
+          )}
+        >
+          <input
+            className={twJoin(
+              'w-full rounded-md border-slate-400 px-2 py-1',
+              !inline && 'w-full',
+              invalid && 'border-2 border-error-500',
+              disabled
+                ? 'cursor-not-allowed border-slate-300 bg-slate-100'
+                : 'bg-white'
+            )}
+            type={type}
+            min={min}
+            max={max}
+            step={step}
+            id={id}
+            disabled={disabled}
+            value={value}
+            onChange={(event) => onChange(event.target.value)}
+          />
+        </div>
+        {message && (
+          <span className={twJoin('text-sm', invalid && 'text-error-500')}>
+            {message}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Input.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  id: PropTypes.string,
+  inline: PropTypes.bool,
+  invalid: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  max: PropTypes.number,
+  message: PropTypes.string,
+  min: PropTypes.number,
+  onChange: PropTypes.func,
+  required: PropTypes.bool,
+  step: PropTypes.number,
+  type: PropTypes.oneOf([
+    'date',
+    'datetime-local',
+    'email',
+    'hidden',
+    'month',
+    'number',
+    'password',
+    'search',
+    'tel',
+    'text',
+    'time',
+    'url',
+    'week',
+  ]),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
---
+++ src/utah-design-system/Input.stories.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Input from './Input';
+
+export default {
+  title: 'Utah Design System/Input',
+  component: Input,
+};
+
+export const Default = () => {
+  const [value, setValue] = useState(123);
+
+  return (
+    <div className="space-y-2">
+      <Input label="Default" />
+      <Input label="Inline" inline className="w-40" />
+      <Input label="Another Inline" inline className="ml-2 w-96" />
+      <Input label="Disabled" disabled />
+      <Input label="Required" required className="w-1/2" />
+      <Input
+        label="Invalid"
+        invalid
+        message="You must enter a valid email address!"
+      />
+      <Input
+        label="Invalid Inline"
+        invalid
+        inline
+        message="You must enter a valid email address!"
+      />
+      <Input label="Another Inline" inline className="ml-2" />
+      <Input
+        label="Controlled"
+        value={value}
+        onChange={setValue}
+        message={`controlled value: ${value}`}
+      />
+      <Input
+        label="Min 1, Max 10, Step 0.5"
+        min={1}
+        max={10}
+        step={0.5}
+        type="number"
+      />
+      <Input label="Date" type="date" />
+    </div>
+  );
+};
