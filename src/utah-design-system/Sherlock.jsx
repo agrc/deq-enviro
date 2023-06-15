295330b34207e3f11f86bc915e324e20a780596c
stdavis <stdavis@utah.gov>
2023-06-15 13:15:17 -0600

    fix(sherlock): clean up unnecessary utility class

--- src/utah-design-system/Sherlock.jsx
+++ src/utah-design-system/Sherlock.jsx
@@ -239,7 +239,7 @@ export default function Sherlock({
   };
 
   return (
-    <div className={twMerge('z-10 w-full', className)}>
+    <div className={twMerge('w-full', className)}>
       {label && <strong>{label}</strong>}
       <div className="relative -m-1 rounded-md p-1 hover:bg-slate-200">
         <div className="absolute bottom-0 left-0 top-0 mx-1 flex w-8 items-center justify-center">
