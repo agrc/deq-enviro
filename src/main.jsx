fa3b91a656e7c8ef99d82787c087664d4a786949
stdavis <stdavis@utah.gov>
2023-10-10 09:58:29 -0600

    fix: simplify firebase config and update measurement id

--- src/main.jsx
+++ src/main.jsx
@@ -19,15 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <FirebaseAppProvider
-          firebaseConfig={{
-            apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-            appId: import.meta.env.VITE_FIREBASE_APP_ID,
-            authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-            messagingSenderId: import.meta.env
-              .VITE_FIREBASE_MESSAGING_SENDER_ID,
-            projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-            storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-          }}
+          firebaseConfig={JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)}
         >
           <App />
         </FirebaseAppProvider>
