18d64bb93abb66a566330d83076ebebcaa52be69
stdavis <stdavis@utah.gov>
2024-02-15 14:37:11 -0700

    chore: change max map zoom from 17 -> 19

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -148,7 +148,7 @@ export default function MapComponent() {
       map: map.current,
       extent: stateOfUtahExtent,
       constraints: {
-        maxZoom: 17,
+        maxZoom: 19,
       },
     });
 
