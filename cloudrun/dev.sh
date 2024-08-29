d23e543df2758cc1a1e13088ad7874209182ae7d
stdavis <stdavis@utah.gov>
2024-08-29 11:15:21 -0600

    chore(download): install deps with arcgis
    I'm not sure why this worked originally but this fixed a "No module named 'cryptography'" error.

--- cloudrun/dev.sh
+++ cloudrun/dev.sh
@@ -2,5 +2,5 @@
 set -e
 
 # this needs to be done after the docker image has been built because of the remote dev extension
-pip install arcgis==2.* --no-deps
+pip install arcgis==2.*
 pip install -e ".[dev]"
