fff32b1d991ec57f3a9d6efbe63245579462441d
stdavis <stdavis@utah.gov>
2024-11-26 11:07:54 -0700

    chore: bump deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,25 @@
 /** @type {import('@storybook/react-vite').StorybookConfig} */
 const config = {
   stories: ['../src/**/*.stories.@(js|jsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
   ],
+
   core: {},
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
