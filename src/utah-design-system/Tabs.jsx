cc6719078de95f380859df7e6b6db29e0689a25b
stdavis <stdavis@utah.gov>
2026-06-10 12:15:39 -0600

    fix: fix tab font size and simplify tailwind classes

--- src/utah-design-system/Tabs.jsx
+++ src/utah-design-system/Tabs.jsx
@@ -7,11 +7,7 @@ import { twMerge } from 'tailwind-merge';
 function Root({ className, ...props }, forwardedRef) {
   return (
     <RadixTabs.Root
-      className={twMerge(
-        'flex',
-        'data-[orientation=horizontal]:flex-col',
-        className,
-      )}
+      className={twMerge('flex', 'orientation-horizontal:flex-col', className)}
       ref={forwardedRef}
       {...props}
     />
@@ -32,8 +28,8 @@ export function List({ className, ...props }) {
     <RadixTabs.List
       className={twMerge(
         'flex border-slate-300 px-3',
-        'data-[orientation=horizontal]:mb-2 data-[orientation=horizontal]:items-center data-[orientation=horizontal]:justify-evenly data-[orientation=horizontal]:border-b-2',
-        'data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:border-l-2',
+        'orientation-horizontal:mb-2 orientation-horizontal:items-center orientation-horizontal:justify-evenly orientation-horizontal:border-b-2',
+        'orientation-vertical:flex-col orientation-vertical:items-start orientation-vertical:border-l-2',
         className,
       )}
       {...props}
@@ -50,18 +46,18 @@ export function Trigger({ className, children, ...props }) {
   return (
     <RadixTabs.Trigger
       className={twMerge(
-        'hover:pointer group/tab relative min-h-[2.5rem] w-full font-bold',
-        'data-[state=active]:before:content=[""] data-[state=active]:text-primary data-[state=active]:before:absolute data-[state=active]:before:z-10 data-[state=active]:before:block data-[state=active]:before:rounded-full data-[state=active]:before:bg-primary',
-        'data-[orientation=horizontal]:mb-1.5 data-[orientation=horizontal]:flex-1 data-[orientation=horizontal]:data-[state=active]:before:-bottom-[.7rem] data-[orientation=horizontal]:data-[state=active]:before:left-0 data-[orientation=horizontal]:data-[state=active]:before:h-2 data-[orientation=horizontal]:data-[state=active]:before:w-full',
-        'data-[orientation=vertical]:flex data-[orientation=vertical]:items-center data-[orientation=vertical]:justify-start',
-        'data-[orientation=vertical]:data-[state=active]:before:-left-[1.05rem] data-[orientation=vertical]:data-[state=active]:before:top-0 data-[orientation=vertical]:data-[state=active]:before:h-full data-[orientation=vertical]:data-[state=active]:before:w-2',
+        'hover:pointer group/tab relative min-h-10 w-full font-bold',
+        'data-[state=active]:before:content=[""] data-[state=active]:text-primary data-[state=active]:before:bg-primary data-[state=active]:before:absolute data-[state=active]:before:z-10 data-[state=active]:before:block data-[state=active]:before:rounded-full',
+        'orientation-horizontal:mb-1.5 orientation-horizontal:flex-1 orientation-horizontal:data-[state=active]:before:bottom-[-0.7rem] orientation-horizontal:data-[state=active]:before:left-0 orientation-horizontal:data-[state=active]:before:h-2 orientation-horizontal:data-[state=active]:before:w-full',
+        'orientation-vertical:flex orientation-vertical:items-center orientation-vertical:justify-start',
+        'orientation-vertical:data-[state=active]:before:top-0 orientation-vertical:data-[state=active]:before:left-[-1.05rem] orientation-vertical:data-[state=active]:before:h-full orientation-vertical:data-[state=active]:before:w-2',
         className,
       )}
       {...props}
     >
-      <h3 className="inline-flex items-center justify-center rounded-full px-2 group-hover/tab:bg-slate-200">
+      <span className="inline-flex items-center justify-center rounded-full px-2 text-lg group-hover/tab:bg-slate-200">
         {children}
-      </h3>
+      </span>
     </RadixTabs.Trigger>
   );
 }
